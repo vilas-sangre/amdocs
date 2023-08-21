ed4182ea0b7850fc819b019212c06bb3cdeb8388
Vilas Sangre <vilas.sangre@eaton.com>
2023-08-21 14:18:02 +0530

    Registration Model

---
+++ Customer.java
@@ -0,0 +1,69 @@
+package com.amdocs.model;
+
+
+
+public class Customer {
+
+	private String firstName;
+
+	
+	private String lastName;
+
+	
+	private Integer freePasses;
+
+	
+	private String postalCode;
+
+	
+	private String email;
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", freePasses=" + freePasses
+				+ ", postalCode=" + postalCode + ", email=" + email + "]";
+	}
+	
+	
+	
+}
